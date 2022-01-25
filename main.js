cedebffed9fdb984a761a8ead56679fa24deb7ef
VladimirParmon <parmon_vladimir@bk.ru>
2022-01-25 03:51:14 +0300

    init: layout

---
+++ main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*******************!*\
+  !*** ./master.ts ***!
+  \*******************/
+
+console.log('working');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9tYWluLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFhO0FBQ2IiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ycy1sYW5nLy4vbWFzdGVyLnRzIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xyXG5jb25zb2xlLmxvZygnd29ya2luZycpO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './master.ts',
+  mode: 'development', // was production
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: 'inline-source-map', // this has changed
+  devServer: {
+    static: './',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: './main.js',
+    path: path.resolve(__dirname, ''),
+  },
+  optimization: {
+    emitOnErrors: true,
+  },
+};
