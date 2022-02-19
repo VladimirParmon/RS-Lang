1322fc0664b1393680ae40244dea6ac6262d3a18
VladimirParmon <parmon_vladimir@bk.ru>
2022-02-19 23:10:49 +0300

    add contact me form

--- navigation/pages/commentsPage.ts
+++ navigation/pages/commentsPage.ts
@@ -3,7 +3,14 @@ import { Page } from "../router";
 
 export const commentsPage: Page = {
   render: () => (`
-
+  <div id="contactMe">
+    <form target="_blank" action="https://formsubmit.co/vladimirredblooded@gmail.com" method="POST">
+      <input type="text" name="name" placeholder="Как вас зовут? (опционально)">
+      <input type="hidden" name="_captcha" value="false">
+      <textarea placeholder="Оставьте ваш комментарий. Никакой регистрации :)" name="message"></textarea>
+      <button type="submit">Отправить</button>
+    </form>
+  </div>
   `),
   afterRender: () => {
     
